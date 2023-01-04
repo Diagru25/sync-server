45e185b2ab41b582d9e5aa3f34d5a4b4c05843c6
diagru25 <diagru2510@gmail.com>
2023-01-04 16:44:53 +0700

    [create] cron job agent status

--- src/agent.js
+++ src/agent.js
@@ -1,1 +1,23 @@
-console.log(new Date())
+import dotenv from 'dotenv';
+import cron from 'node-cron';
+import axios from 'axios';
+import os from 'os';
+
+dotenv.config()
+
+const task = cron.schedule('*/5 * * * *', () => {
+    sendStatusAgent()
+});
+
+const sendStatusAgent = async () => {
+    try {
+        const computerName = os.hostname();
+        await axios.post(`${process.env.SERVER_URL}/api/agents/status`, { name: computerName });
+    }
+    catch(error) {
+        console.log("Agent: ", error);
+        return;
+    }
+}
+
+task.start();
