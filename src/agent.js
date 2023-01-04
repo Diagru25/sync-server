471da510c9302069a9914d2936e3b1c15c47ada3
diagru25 <diagru2510@gmail.com>
2023-01-04 16:45:51 +0700

    [fix] some commma

--- src/agent.js
+++ src/agent.js
@@ -5,19 +5,19 @@ import os from 'os';
 
 dotenv.config()
 
-const task = cron.schedule('*/5 * * * *', () => {
-    sendStatusAgent()
-});
-
 const sendStatusAgent = async () => {
     try {
         const computerName = os.hostname();
         await axios.post(`${process.env.SERVER_URL}/api/agents/status`, { name: computerName });
     }
-    catch(error) {
+    catch (error) {
         console.log("Agent: ", error);
         return;
     }
 }
+
+const task = cron.schedule('*/5 * * * *', () => {
+    sendStatusAgent();
+});
 
 task.start();
