7774a5383d6e0e652c8353cb4a9054083676e1e2
diaru25 <diaru2510@gmail.com>
2023-02-05 21:25:46 +0700

    [edit] api tunels ngrok

--- src/agent.js
+++ src/agent.js
@@ -1,33 +1,41 @@
-import dotenv from 'dotenv';
-import cron from 'node-cron';
-import axios from 'axios';
-import os from 'os';
+import dotenv from "dotenv";
+import cron from "node-cron";
+import axios from "axios";
+import os from "os";
 
-dotenv.config()
+dotenv.config();
 
 const sendStatusAgent = async () => {
-    try {
-        // get ngrok info
-        const ResNgrok = await axios.get('http://localhost:4040/api/tunnels');
-        const {tunnels} = ResNgrok.data;
-        if(tunnels.length === 0) {
-            console.log('ngrok is not running!');
-            return;
-        }
-        else {
-            const { public_url } = tunnels[0];
-            const computerName = os.hostname();
-            await axios.post(`${process.env.SERVER_URL}/api/agents/status`, { name: computerName, publicUrl: public_url });
-        }  
-    }
-    catch (error) {
-        console.log("Agent: ", error);
+  try {
+    // get ngrok info
+    const ResNgrok = await axios.get("http://localhost:4040/api/tunnels");
+    const { tunnels } = ResNgrok.data;
+    if (tunnels.length === 0) {
+      console.log("ngrok is not running!");
+      return;
+    } else {
+      const foundIndex = tunnels.findIndex((el) => el.name === "ssh");
+      if (foundIndex !== -1) {
+        const { public_url } = tunnels[foundIndex];
+        const computerName = os.hostname();
+        await axios.post(`${process.env.SERVER_URL}/api/agents/status`, {
+          name: computerName,
+          publicUrl: public_url,
+        });
         return;
+      } else {
+        console.log("ngrok SSH not open!");
+        return;
+      }
     }
-}
+  } catch (error) {
+    console.log("Agent: ", error);
+    return;
+  }
+};
 
-const task = cron.schedule('*/10 * * * * *', () => {
-    sendStatusAgent();
+const task = cron.schedule("*/10 * * * * *", () => {
+  sendStatusAgent();
 });
 
 task.start();
