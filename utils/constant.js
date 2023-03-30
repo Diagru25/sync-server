5b25765329702b85f5f53d7909768b7e94fedcc0
diagru25 <diagru2510@gmail.com>
2023-03-30 22:28:22 +0700

    [change] chnage regex ver 2

--- utils/constant.js
+++ utils/constant.js
@@ -1,2 +1,3 @@
-export const REGEX_SPLIT = /(?=[A-Z]\d{2}\s\d{4}\s\d{2}\s\d{2})/g;
+//export const REGEX_SPLIT = /(?=[A-Z]\d{2}\s\d{4}\s\d{2}\s\d{2})/g;
+export const REGEX_SPLIT = /(\d{1,2}\s\d{2}\s\s\d{1,2}\s\d{1,2})/g;
 export const REGEX_EXT = /(\.\d{2}n)|(.nav)/g;
