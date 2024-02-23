919a7395d6ecc55da387cb860a7827a8ae1abd9c
diagru25 <diagru2510@gmail.com>
2024-02-23 16:23:58 +0700

    [fix] token

--- src/middleware.js
+++ src/middleware.js
@@ -4,16 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  let _token =
+    req.body.token || req.query.token || req.headers["authorization"];
+
+  let token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
+    console.log(token, process.env.TOKEN_KEY);
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    req.user = decoded;
+    // req.user = decoded;
   } catch (err) {
+    console.log(err);
     return res.status(401).send("Invalid Token");
   }
   return next();
